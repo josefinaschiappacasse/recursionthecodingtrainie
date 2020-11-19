3ff1172647dba51bdf983d413f2a9a3ec787d570
josefinaschiappacasse <69818086+josefinaschiappacasse@users.noreply.github.com>
2020-11-19 20:47:11 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,30 +1,29 @@
 //https://www.youtube.com/watch?v=0jjeOYMjmDU
-let sketch;
+
 var angle = 0
 var s1;
 
 function setup() {
-  sketch = createCanvas(400, 400);
-  sketch.parent("p5");
-  s1 = createSlider(0, TWO_PI, PI / 4, 0.01);
-  s1.parent("p5");
+  createCanvas(400, 400);
+  s1 = createSlider(0, TWO_PI, PI / 9, 0.01);
 }
 
 function draw() {
-  background(0);
+  background(255);
+  fill(255, 45, 78, 100);
   angle = s1.value();
+  translate(200, 400);
   stroke(0);
-  translate(200, height);
-  stroke(255);
   branch(30);
-  stroke(255)
-  branch(70);
-  stroke("pink");
+  //stroke(0)
+  //branch(70);
+  stroke(0);
   branch(103);
+  
 }
 
 function branch(len) {
-  line(0, 0, 0, -len);
+  ellipse(0, 10, 30, -len);
   translate(0, -len);
   if (len > 4) { 
     push();
